f9b55558b98980f8b5a8771254f3a73f3408c3f4
Steven Cheung <stevencheung@hotmail.co.uk>
2022-11-23 18:46:58 +0000

    added h2 in TaskListItems

--- tasks_app/src/components/TaskListItems.vue
+++ tasks_app/src/components/TaskListItems.vue
@@ -1,15 +1,18 @@
 <template>
-  <ul class="taskListItems">
-    <li
-      v-for="(task, index) in taskList"
-      :id="`task-${task.id}`"
-      :key="task.id"
-    >
-      <p>Your task: {{ task.taskDescription }}</p>
-      <p>Priority Number: {{ task.priorityNumber }}</p>
-      <button @click="$emit('remove', index)">Remove</button>
-    </li>
-  </ul>
+  <div>
+    <h2>View your tasks</h2>
+    <ul class="taskListItems">
+      <li
+        v-for="(task, index) in taskList"
+        :id="`task-${task.id}`"
+        :key="task.id"
+      >
+        <p>Your task: {{ task.taskDescription }}</p>
+        <p>Priority Number: {{ task.priorityNumber }}</p>
+        <button @click="$emit('remove', index)">Remove</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
